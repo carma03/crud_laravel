3b6e30c9dfbc778fb1ef0837fe76e1bd57b5960c
carma03 <carlospuello03@gmail.com>
2015-07-13 00:25:21 -0500

    add logo in home

--- resources/views/home_view.blade.php
+++ resources/views/home_view.blade.php
@@ -8,11 +8,12 @@
 @endif @section('content')
 <div class="container_">
     <div class="title_home_content">
-        <div class="title_home">Esta es una demo de un CRUD en Laravel</div>
+        <div class="title_home">Esta es una demo de un CRUD en Laravel 5.1</div>
         <!-- <img src="http://lorempicsum.com/futurama/400/200/7" alt=""> -->
-        <div class="img">
-
+        <div class="img_home">
+            <img src="images/laravel_logo.png" alt="" class="img img-responsive center-block">
         </div>
+
     </div>
 </div>
 @endsection
